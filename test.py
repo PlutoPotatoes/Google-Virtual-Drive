b2597846f224fe915e0af4c5ced42aa49eb3fae4
PlutoPotatoes <ryan.morrell@gmail.com>
2025-10-15 11:04:52 -0700

    readme update

--- test.py
+++ test.py
@@ -8,10 +8,10 @@ origin = "100 Main St 10th Floor, Los Angeles, CA 90012"
 destination = "4884 Eagle Rock Blvd, Los Angeles, CA 90041"
 
 #Use Directions API
-drive.drive_directions(origin, destination, API_KEY, datafile='drive1.csv')
+#drive.drive_directions(origin, destination, API_KEY, datafile='drive1.csv')
 
 #Use Routes API
 #drive.drive_route(origin, destination, API_KEY, minStep=30, fov = 90, datafile='drive1.csv')
 
 #Use CSV Coordinates
-#drive.csv_drive("GrandAv.csv", API_KEY, pitchAngle=5, fov=90, datafile='drive1.csv')
+drive.csv_drive("GrandAv.csv", API_KEY, pitchAngle=5, fov=90, datafile='drive1.csv')
